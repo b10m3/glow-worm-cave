0c436f7b0d561825f11dba1787f5fb329e86915e
Douglas Barton <douglas.barton@gmail.com>
2016-02-20 19:44:00 -0800

    adds life to glow_worm

--- glow_worm.ino
+++ glow_worm.ino
@@ -22,12 +22,15 @@
 #define LEDS_PIN_4    13
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
-#define NUM_LEDS    4
-#define NUM_LEDS_PER_STRIP 1
 #define NUM_PIRS    4
+#define NUM_LEDS    1
+#define NUM_LED_STRIPS 4
 CRGB leds[NUM_LEDS];
+CLEDController *controllers[NUM_LED_STRIPS];
+
 int ledhues[NUM_LEDS];
 int ledvals[NUM_LEDS];
+int led_pins[NUM_LEDS];
 int pir_pins[NUM_PIRS];
 int pir_states[NUM_PIRS];
 int stimulated_pirs[NUM_PIRS];
@@ -36,7 +39,7 @@ int MAX_PIXEL_VAL_DIFF=20;
 int randomaddhue=0;
 int randomaddsat=0;
 
-#define BRIGHTNESS          96
+#define gBRIGHTNESS          96
 #define FRAMES_PER_SECOND  120
 
 void setup() {
@@ -47,7 +50,12 @@ void setup() {
   pir_pins[2]=PIR_PIN_3;
   pir_pins[3]=PIR_PIN_4;
   
-  
+  // tell FastLED about the LED strip configuration
+  controllers[0] = &FastLED.addLeds<LED_TYPE,LEDS_PIN_1>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  controllers[1] = &FastLED.addLeds<LED_TYPE,LEDS_PIN_2>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  controllers[2] = &FastLED.addLeds<LED_TYPE,LEDS_PIN_3>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  controllers[3] = &FastLED.addLeds<LED_TYPE,LEDS_PIN_4>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
   for (int i = 0; i < NUM_PIRS; ++i)
   {
     stimulated_pirs[i] = 255; //goes 0-255
@@ -57,19 +65,12 @@ void setup() {
   
   delay(3000); // 3 second delay for recovery
   for (int i = 0; i < NUM_LEDS; i++) {
+    //set the leds
+    leds[i]=CHSV(200,255,stimulated_pirs[i]);
     ledhues[i]=180;
     ledvals[i]=200;
   }
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,LEDS_PIN_1,COLOR_ORDER>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE,LEDS_PIN_2,COLOR_ORDER>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE,LEDS_PIN_3,COLOR_ORDER>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE,LEDS_PIN_4,COLOR_ORDER>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   
-  // set master brightness control
-  FastLED.setBrightness(BRIGHTNESS);
-
   // Begin serial dump for debug
   Serial.begin(9600);
 }
@@ -79,41 +80,61 @@ void loop()
 {
   for (int i = 0; i < NUM_PIRS; ++i)
   {
-    pir_states[i] = digitalRead(pir_pins[i]);
-  }
-  assess_excitement();
-  update_blinkies_based_on_excitement();
-  
-  // send the 'leds' array out to the actual LED strip
-  FastLED.show();  
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND);
-}
+    pir_states[i] = digitalRead(pir_pins[i]); // get state of infrared
+    Serial.println(i);
+    Serial.println(pir_states[i]);
+    Serial.println(stimulated_pirs[i]); //debug, print to serial
 
-void assess_excitement()
-{
-  for (int i = 0; i < NUM_PIRS; ++i)
-  {
-    Serial.println(pir_states[i] + stimulated_pirs[i]);
-    
-    if (pir_states[i] == HIGH && stimulated_pirs[i] > 0)
+    /* ===============================
+     *  Patterns for life
+     * ===============================
+     * If movement sensed then retreat!
+     * pir_states go from 0 off to 255 full brightness
+     * Randomly fade to show activity, glitch out in their matrix
+     */
+
+     /* ===============================
+      * pir_states
+      * ===============================
+      */
+    if (pir_states[i] == HIGH && stimulated_pirs[i] > 128)
     {
-      stimulated_pirs[i] = stimulated_pirs[i] - 1; //goes 0-255
+      stimulated_pirs[i] = stimulated_pirs[i] - 7;
+      controllers[i]->showLeds(stimulated_pirs[i]);
+    }
+    else if (pir_states[i] == HIGH && stimulated_pirs[i] > 20)
+    {
+      stimulated_pirs[i] = stimulated_pirs[i] - 1;
+      controllers[i]->showLeds(stimulated_pirs[i]);
+    }
+    else if (pir_states[i] == LOW && stimulated_pirs[i] < 128)
+    {
+       stimulated_pirs[i] = stimulated_pirs[i] + 4;
+       controllers[i]->showLeds(stimulated_pirs[i]);
     }
     else if (pir_states[i] == LOW && stimulated_pirs[i] < 255)
     {
-       stimulated_pirs[i] = stimulated_pirs[i] + 1; //goes 0-255
+       stimulated_pirs[i] = stimulated_pirs[i] + 1;
+       controllers[i]->showLeds(stimulated_pirs[i]);
+    }
+    
+    /*===============================
+     * Random activity
+     * ===============================
+     */
+    if( random8() < 80) {
+        leds[i]=CHSV(200,255,stimulated_pirs[i]); //randomly turn light brightness back to state
+    }
+    if( random8() < 20 && stimulated_pirs[i] > 30) {
+        controllers[i]->showLeds(stimulated_pirs[i] - 20); //randomly turn light brightness off for glitch
+    }
+    if( random8() < 20 && stimulated_pirs[i] > 60) {
+        leds[i]=CHSV(200,255,stimulated_pirs[i] - 50); //randomly turn light brightness off for glitch
     }
   }
-}
-
-
-void update_blinkies_based_on_excitement()
-{
-  for (int i = 0; i < NUM_PIRS; ++i)
-  {
-    leds[i]=CHSV(180,255,stimulated_pirs[i]);
-  }
+  delay(30);
+  
+  
 }
 
 
